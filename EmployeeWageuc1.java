ffe9b455198c4e00eb6e646555dd9b41fc4ab783
ShruthinSatheesh300 <vishnuofch@gmail.com>
2021-12-17 22:12:57 +0530

    Added:code for Checking wether Emp is preset or absent

---
+++ EmployeeWageuc1.java
@@ -0,0 +1,14 @@
+public class EmployeeWageuc1 {
+    public static void main(String[] args) {
+ 
+  int Emp_present = 1;
+  double empcheck = Math.floor(Math.random() * 10) % 2;
+  if (empcheck == Emp_present) {
+	  System.out.println("Employee is present");
+  }
+  else {
+	System.out.println("Employee is absent");
+  }
+  }
+  }
+ 	
